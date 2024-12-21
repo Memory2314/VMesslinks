982b597000e6ef3aca2ba5869158127c607e95b3
Memory <134070804+Memory2314@users.noreply.github.com>
2024-12-21 09:50:50 +0800

    更新 get_vmess.py

--- utils/get_vmess.py
+++ utils/get_vmess.py
@@ -1,45 +1,31 @@
-import sys, subprocess, pyaes, requests, base64, json, binascii
+import requests
 from datetime import datetime
 
-def f(g, d, e):
-    h = pyaes.AESModeOfOperationCBC(d, iv=e)
-    i = b''.join(h.decrypt(g[j:j+16]) for j in range(0, len(g), 16))
-    return i[:-i[-1]]
+# 获取今天的日期
+year = datetime.now().year
+month = datetime.now().strftime("%m")
+day = datetime.now().strftime("%d")
 
-a = 'http://api.skrapp.net/api/serverlist'
-b = {
-    'accept': '/',
-    'accept-language': 'zh-Hans-CN;q=1, en-CN;q=0.9',
-    'appversion': '1.3.1',
-    'user-agent': 'SkrKK/1.3.1 (iPhone; iOS 13.5; Scale/2.00)',
-    'content-type': 'application/x-www-form-urlencoded',
-    'Cookie': 'PHPSESSID=fnffo1ivhvt0ouo6ebqn86a0d4'
-}
-c = {'data': '4265a9c353cd8624fd2bc7b5d75d2f18b1b5e66ccd37e2dfa628bcb8f73db2f14ba98bc6a1d8d0d1c7ff1ef0823b11264d0addaba2bd6a30bdefe06f4ba994ed'}
-d = b'65151f8d966bf596'
-e = b'88ca0f0ea1ecf975'
+# 构建 URL，日期部分会自动替换为当前日期
+url = f"https://clashnode.cc/uploads/{year}/{month}/0-{year}{month}{day}.txt"
 
-j = requests.post(a, headers=b, data=c)
+# 发送 HTTP 请求获取链接内容
+try:
+    v2ray_response = requests.get(url+'.txt')
+    clash_response = requests.get(url+'.yaml')
+    response.raise_for_status()  # 检查请求是否成功
 
-if j.status_code == 200:
-    k = j.text.strip()
-    l = binascii.unhexlify(k)
-    m = f(l, d, e)
-    n = json.loads(m)
-    vmess = ""
-    for o in n['data']:
-        p = f"aes-256-cfb:{o['password']}@{o['ip']}:{o['port']}"
-        q = base64.b64encode(p.encode('utf-8')).decode('utf-8')
-        r = f"ss://{q}#{o['title']}"
-        vmess += f"{r}\n"
+    print(f"成功获取到内容 (日期: {current_date}):")
+    v2ray_node = v2ray_response.text
+    print(v2ray_node)
 
-# Base64 编码
-encoded_vmess = base64.b64encode(vmess.encode('utf-8')).decode('utf-8')
+except requests.exceptions.RequestException as e:
+    print(f"请求失败: {e}")
 
 # 将去重后的节点信息保存到文件
-with open("./links/vmess", "w") as f:
-    f.write(vmess)
-with open("./links/base64", "w") as f:
-    f.write(encoded_vmess)
+with open("./links/v2ray", "w") as f:
+    f.write(v2ray_node)
+with open("./links/clash", "w") as f:
+    f.write(clash_response)
 
 print("节点信息已保存")
